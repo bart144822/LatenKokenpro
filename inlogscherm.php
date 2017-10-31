987f6d8a1cbd9ff553a36c2d667ec110cc0e10ba
Triplays <33252538+Triplays@users.noreply.github.com>
2017-10-31 12:03:18 +0100

    Inlogscherm

---
+++ inlogscherm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Inlogscherm</title> 
+</head> 
+<body> 
+<form Action="inlogscherm.php" method="POST">
+<input type="text" name="gebruikersNaam"><br>
+<input type="password" name="wachtwoord"><p>
+
+<input type="submit" value="Log in">
+
+</form>
+<form action="registratie.php">
+<input type="submit" Value="registreer">
+</form>
+</body> 
+</html> 
+	
