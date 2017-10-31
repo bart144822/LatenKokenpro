f1260f03b18bedb4133591b81a8c9fcba46c13b1
StijnVisscher <33252434+StijnVisscher@users.noreply.github.com>
2017-10-31 14:47:53 +0100

    Restaurant pagina temp

---
+++ Restaurant1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title></title> 
+</head> 
+<body> 
+<?php //include 'Dataconnectie.php';?>
+<?php //include 'head.html';?>
+<?php //$query = "SELECT * FROM items   ";
+///$result = mysqli_query($db, $query);
+//if (!$result) { 
+//die("Database query failed.");
+//}
+?>
+<h1 align="center">Restaurant naam</h1>
+<h2 align="right">Foto</h2>
+<link href="style1.css" rel="stylesheet" type="text/css"/>
+<div id="content">
+<fieldset>
+<legend>Restaurant menu:</legend>
+<br>
+<table>
+<form method="POST" action="*">
+<tr><td></td>
+<td></td></tr>
+</table>
+<br><br>
+</fieldset>
+</div>
+<?php //include 'Footer.php';?>
+<?php //include 'Dataeinde.php';?>
+</body> 
+</html> 
