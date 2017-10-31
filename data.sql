06c14ea45876d11f6f32e21bb2e16d595cbf79cc
bart144822 <33252085+bart144822@users.noreply.github.com>
2017-10-31 14:10:53 +0100

    fixed

--- data.sql
+++ data.sql
@@ -1,64 +1,192 @@
-CREATE TABLE items (
-product_id INT NOT NULL,
-restaurant_id INT NOT NULL,
-product_naam VARCHAR(50) NOT NULL,
-prijs INT NOT NULL,
-restaurant_restaurant_id INT NOT NULL
-);
-ALTER TABLE items ADD CONSTRAINT items_pk PRIMARY KEY ( product_id,restaurant_id );
-CREATE TABLE klant (
-klant_id INT NOT NULL,
-voornaam VARCHAR(20) NOT NULL,
-achternaam VARCHAR(25) NOT NULL,
-straatnaam VARCHAR(30) NOT NULL,
-postcode VARCHAR(6) NOT NULL,
-huisnummer VARCHAR(6) NOT NULL,
-plaatsnaam VARCHAR(30) NOT NULL,
-email VARCHAR(40) NOT NULL,
-wachtwoord CHAR(32) NOT NULL
-);
-ALTER TABLE klant ADD CONSTRAINT klant_pk PRIMARY KEY ( klant_id );
-CREATE TABLE order_items (
-bestelling_id INT NOT NULL,
-restaurant_id INT NOT NULL,
-product_id INT NOT NULL,
-items_product_id INT NOT NULL,
-items_restaurant_id INT NOT NULL,
-orders_klant_id INT NOT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 31 okt 2017 om 14:08
+-- Serverversie: 10.1.16-MariaDB
+-- PHP-versie: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-ALTER TABLE order_items
-ADD CONSTRAINT order_items_pk PRIMARY KEY ( restaurant_id,product_id,bestelling_id );
-CREATE TABLE orders (
-order_id INT NOT NULL,
-klant_id INT NOT NULL,
-order_date DATE NOT NULL,
-order_items INT NOT NULL
-);
-ALTER TABLE orders ADD CONSTRAINT orders_pk PRIMARY KEY ( order_id,klant_id );
-CREATE TABLE restaurant (
-restaurant_id INT NOT NULL,
-restaurantnaam VARCHAR(50) NOT NULL,
-straatnaam VARCHAR(30) NOT NULL,
-postcode VARCHAR(6) NOT NULL,
-huisnummer VARCHAR(6) NOT NULL,
-plaatsnaam VARCHAR(30) NOT NULL,
-email VARCHAR(40) NOT NULL,
-telefoonnummer INT NOT NULL
-);
-ALTER TABLE restaurant ADD CONSTRAINT restaurant_pk PRIMARY KEY ( restaurant_id );
-ALTER TABLE items
-ADD CONSTRAINT items_restaurant_fk FOREIGN KEY ( restaurant_restaurant_id )
-REFERENCES restaurant ( restaurant_id );
-ALTER TABLE order_items
-ADD CONSTRAINT order_items_items_fk FOREIGN KEY ( items_product_id,items_restaurant_id )
-REFERENCES items ( product_id,restaurant_id );
-ALTER TABLE order_items
-ADD CONSTRAINT order_items_orders_fk FOREIGN KEY ( bestelling_id,orders_klant_id )
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `asdasdasdasdasd`
+--
 
-REFERENCES orders ( order_id,klant_id );
-ALTER TABLE orders
-ADD CONSTRAINT orders_klant_fk FOREIGN KEY ( klant_id )
-REFERENCES klant ( klant_id );
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `items`
+--
+
+CREATE TABLE `items` (
+  `product_id` int(11) NOT NULL,
+  `restaurant_id` int(11) NOT NULL,
+  `product_naam` varchar(50) NOT NULL,
+  `prijs` int(11) NOT NULL,
+  `restaurant_restaurant_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `klant`
+--
+
+CREATE TABLE `klant` (
+  `klant_id` int(11) NOT NULL,
+  `voornaam` varchar(20) NOT NULL,
+  `achternaam` varchar(25) NOT NULL,
+  `straatnaam` varchar(30) NOT NULL,
+  `postcode` varchar(6) NOT NULL,
+  `huisnummer` varchar(6) NOT NULL,
+  `plaatsnaam` varchar(30) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `wachtwoord` char(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `klant_id` int(11) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_items` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `bestelling_id` int(11) NOT NULL,
+  `restaurant_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `items_product_id` int(11) NOT NULL,
+  `items_restaurant_id` int(11) NOT NULL,
+  `orders_klant_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `restaurant_id` int(11) NOT NULL,
+  `restaurantnaam` varchar(50) NOT NULL,
+  `straatnaam` varchar(30) NOT NULL,
+  `postcode` varchar(6) NOT NULL,
+  `huisnummer` varchar(6) NOT NULL,
+  `plaatsnaam` varchar(30) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `telefoonnummer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`product_id`,`restaurant_id`),
+  ADD KEY `items_restaurant_fk` (`restaurant_restaurant_id`);
+
+--
+-- Indexen voor tabel `klant`
+--
+ALTER TABLE `klant`
+  ADD PRIMARY KEY (`klant_id`);
+
+--
+-- Indexen voor tabel `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`,`klant_id`),
+  ADD KEY `orders_klant_fk` (`klant_id`);
+
+--
+-- Indexen voor tabel `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`bestelling_id`),
+  ADD KEY `order_items_items_fk` (`items_product_id`,`items_restaurant_id`),
+  ADD KEY `order_items_orders_fk` (`bestelling_id`,`orders_klant_id`);
+
+--
+-- Indexen voor tabel `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`restaurant_id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `items`
+--
+ALTER TABLE `items`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `klant`
+--
+ALTER TABLE `klant`
+  MODIFY `klant_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `bestelling_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_restaurant_fk` FOREIGN KEY (`restaurant_restaurant_id`) REFERENCES `restaurant` (`restaurant_id`);
+
+--
+-- Beperkingen voor tabel `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_klant_fk` FOREIGN KEY (`klant_id`) REFERENCES `klant` (`klant_id`);
+
+--
+-- Beperkingen voor tabel `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_items_fk` FOREIGN KEY (`items_product_id`,`items_restaurant_id`) REFERENCES `items` (`product_id`, `restaurant_id`),
+  ADD CONSTRAINT `order_items_orders_fk` FOREIGN KEY (`bestelling_id`,`orders_klant_id`) REFERENCES `orders` (`order_id`, `klant_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
