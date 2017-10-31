bbd8d67ff17aee742927f8294083e1726f5ee57c
sybkuiper <33252799+sybkuiper@users.noreply.github.com>
2017-10-31 12:14:23 +0100

    Add files via upload

---
+++ winkelmandje.php
@@ -0,0 +1,11 @@
+<html>
+<title>Winkelmandje</title>
+<body>
+<?php
+include 'head.html';
+include 'style1.css';
+?>
+<h1 align="center">Restaurant naam</h1>
+<h2 align="right">Foto</h2>
+</body>
+</html>	
