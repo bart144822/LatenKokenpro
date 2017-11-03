084a2c4631a734b4e17a32d5d486f68d073747c1
StijnVisscher <33252434+StijnVisscher@users.noreply.github.com>
2017-11-03 12:23:39 +0100

    Restaurant 1

--- Restaurant1.php
+++ Restaurant1.php
@@ -5,13 +5,13 @@
 <link rel="stylesheet" type="text/css" href="restaurantpaginastyle.css"> 
 </head> 
 <body> 
-<?php //include 'Dataconnectie.php';?>
-<?php //include 'head.html';?>
-<?php //$query = "SELECT * FROM items WHERE restaurant_id = 1 ";
-///$result = mysqli_query($db, $query);
-//if (!$result) { 
-//die("Database query failed.");
-//}
+<?php include 'Dataconnectie.php';?>
+<?php include 'head.html';?>
+<?php $query = "SELECT * FROM items WHERE restaurant_restaurant_id = 1 ";
+$result = mysqli_query($db, $query);
+if (!$result) { 
+die("Database query failed.");
+}
 ?>
 <h1 align="center">Mccluskey</h1>
 <h2 align="right">Foto</h2>
@@ -20,16 +20,19 @@
 <legend>Restaurant menu:</legend>
 <br>
 <table>
-<form method="POST" action="*">
-<?php //while($row = mysqli_fetch_assoc($result)){ 
-//echo "<tr>"."<td>". $item = $row["product_naam"] $prijs = $row["prijs"]."</td>"."</tr>";
-//}
+<form method="POST" action="">
+<?php while($row = mysqli_fetch_assoc($result)){ 
+echo "<tr>"."<td>"."<input type='hidden' value='['product_id']'>".$item = $row["product_naam"] ."<td>"."<td>"."<td>"."<td>"."€".$prijs = $row["prijs"]."<td>"."<td>"."<input type='number' min='0' max='99' name='aantal'>"."</td>"."</tr>";
+}
 ?>
 </table>
+<input type='submit' value='Toevoegen aan bestelling'>
+<?php var_dump($_POST); ?>
 <br><br>
 </fieldset>
+</from>
 </div>
 <?php //include 'Footer.php';?>
-<?php //include 'Dataeinde.php';?>
+<?php include 'Dataeinde.php';?>
 </body> 
 </html> 
