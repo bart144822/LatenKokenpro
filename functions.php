5f7e6e4106bfc4fab9fea732726cd9158b09c0c1
bart144822 <33252085+bart144822@users.noreply.github.com>
2017-11-03 11:26:44 +0100

    function distance

--- functions.php
+++ functions.php
@@ -19,9 +19,35 @@ function GetAdres($adres){
       return"ingevulde adres klopt niet";
   }else{
       //var_dump($json);
-      return ($json['results'][0]['geometry']['location']['lat'].",".$json['results'][0]['geometry']['location']['lng']);
+      return ($json['results'][0]['geometry']['location']['lat']." ".$json['results'][0]['geometry']['location']['lng']);
   }
 
 }
 
-//echo GetAdres("meerweg 35 9606 pm kropswolde");
+$locationr=GetAdres("meerweg 3 9606 pm kropswolde");
+
+$locationu=GetAdres("meerweg 35 9606 pm kropswolde");
+
+list($location1, $location2)= explode(' ',$locationu);
+list($location3, $location4)= explode(' ',$locationr);
+
+//echo "$location1. $location2. $location3. $location4";
+
+
+//var_dump($locationu);
+function distance($lat1, $lon1, $lat2, $lon2) {
+
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $miles = $dist * 60 * 1.1515;
+    //$unit = strtoupper($unit);
+    return ($miles * 1.609344);
+
+}
+
+
+echo distance($location1, $location2, $location3, $location4) . " Kilometers<br>";
+
+
