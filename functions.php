346c5864f3d9b8572a0a825c8aea5fbb46bcb467
bart144822 <33252085+bart144822@users.noreply.github.com>
2017-11-07 13:39:35 +0100

    functie

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * User: bart
  * Date: 11/02/2017
  * Time: 11:04
- */
+ *///deze functie maakt van een adres cordinaten
 function GetAdres($adres){
 
     $adres = str_replace(" ", "+", $adres); // alle spaties worden een +
@@ -15,27 +15,17 @@ function GetAdres($adres){
     $response = file_get_contents($url);
 
     $json = json_decode($response,TRUE); //het resultaat word in een array gezet
-  if ($json["status"]=="ZERO_RESULTS"){
-      //var_dump($json);
-      return"ingevulde adres klopt niet";
-  }else{
-      //var_dump($json);
-      return ($json['results'][0]['geometry']['location']['lat']." ".$json['results'][0]['geometry']['location']['lng']);
-  }
+    if ($json["status"]=="ZERO_RESULTS"){
+        //var_dump($json);
+        return"ingevulde adres klopt niet";
+    }else{
+        //var_dump($json);
+        return ($json['results'][0]['geometry']['location']['lat']." ".$json['results'][0]['geometry']['location']['lng']);
+    }
 
 }
 
-//$locationr=GetAdres("meerweg 3 9606 pm kropswolde");
-//$ad="meerweg 35 9606 pm kropswolde";
-//echo GetAdres($ad);
-
-//list($location1, $location2)= explode(' ',$locationu);
-//list($location3, $location4)= explode(' ',$locationr);
-
-//echo "$location1. $location2. $location3. $location4";
-
-
-//var_dump($locationu);
+// deze functie geeft van 2 gegeven cordinaten het verschil in kilometers
 function distance($lat1, $lon1, $lat2, $lon2) {
 
     $theta = $lon1 - $lon2;
@@ -49,42 +39,42 @@ function distance($lat1, $lon1, $lat2, $lon2) {
 }
 
 
-//echo distance($location1, $location2, $location3, $location4) . " Kilometers<br>";
+//echo
 function restaurantselectie($adres){
-$laag="";
-include "Dataconnectie.php";   
-$query = "SELECT restaurant_id, straatnaam, huisnummer, postcode, plaatsnaam, restaurantnaam FROM restaurant ";
-$result = mysqli_query($db, $query);
-if (!$result) { 
-die("Database query failed.");
-}
-$klantadres = GetAdres($adres);
+    $laag="";
+    include "Dataconnectie.php";
+    $query = "SELECT restaurant_id, straatnaam, huisnummer, postcode, plaatsnaam, restaurantnaam FROM restaurant ";
+    $result = mysqli_query($db, $query);
+    if (!$result) {
+        die("Database query failed.");
+    }
+    $klantadres = GetAdres($adres);
 //$Radres=array("");
-while($row = mysqli_fetch_assoc($result)){
+    while($row = mysqli_fetch_assoc($result)){
 
 
-    $Radres=GetAdres($row['straatnaam'], $row['huisnummer'], $row['postcode'], $row['plaatsnaam']);
+        $Radres=GetAdres($row['straatnaam'], $row['huisnummer'], $row['postcode'], $row['plaatsnaam']);
 //var_dump($Radres);
 
 
-    list($location1, $location2)= explode(' ',$klantadres);
-    list($location3, $location4)= explode(' ',$Radres);
-    echo"<br />";
-    $distance=distance($location1, $location2, $location3, $location4);
-    //echo $distance;
-    if ($distance<10){
-        $laag.=$row['restaurant_id'];
-        $laag.=" OR ";
+        list($location1, $location2)= explode(' ',$klantadres);
+        list($location3, $location4)= explode(' ',$Radres);
+        echo"<br />";
+        $distance=distance($location1, $location2, $location3, $location4);
+        //echo $distance;
+        if ($distance<10){
+            $laag.=$row['restaurant_id'];
+            $laag.=", ";
+        }
     }
+    return substr($laag, 0, -2);
 }
- return substr($laag, 0, -3);
-}
 
 
 
 
-// $distance=distance($location1, $location2, $location3, $location4);
+//$distance=distance($location1, $location2, $location3, $location4);
 
 
-echo restaurantselectie("Oude Ebbingestraat 86 9712 HM Groningen");
+//echo restaurantselectie("Oude Ebbingestraat 86 9712 HM Groningen");
 // //include 'Dataeinde.php';?>
