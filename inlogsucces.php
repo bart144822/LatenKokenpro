89f4eca64277bac0ed84263bb85ed63d39c4d6bf
Triplays <33252538+Triplays@users.noreply.github.com>
2017-11-07 15:21:21 +0100

    esredtfyguhiuojkipol

---
+++ inlogsucces.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title></title> 
+</head> 
+<body> 
+<table>
+<form>
+<h2>Het inloggen was succesvol </h2><br/>
+<input type="button" value="Home" formaction="index.php">
+</form>
+</table>
+</body> 
+</html> 
