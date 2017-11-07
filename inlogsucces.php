44516bc2e3b2dbefca6df34584aa0c78aea83035
Triplays <33252538+Triplays@users.noreply.github.com>
2017-11-07 16:16:10 +0100

    inlog succes/fail

--- inlogsucces.php
+++ inlogsucces.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html> 
 <html> 
 <head> 
-<title></title> 
+<title>inlogsucces</title> 
 </head> 
 <body> 
+<link rel="stylesheet" type="text/css" href="loginsucces.css">
 <table>
 <form>
-<h2>Het inloggen was succesvol </h2><br/>
-<input type="button" value="Home" formaction="index.php">
+<h2>Het inloggen was succesvol </h2>
+<input type="submit" formaction="index.php" ID="button" value="Home">
 </form>
 </table>
 </body> 
---
+++ registratiefail.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>Registratiefail</title> 
+</head> 
+<body>
+<form> 
+<table>
+<link rel="stylesheet" type="text/css" href="registratiecheckbuttonsstyle.css">
+<tr> 
+<td><input ID="failbutton" type= "button" value ="Probeer opnieuw" formaction="inlogscherm.php">
+</td>
+<td><input ID="failbutton" type="button" value="anuleer" formaction="index.php"></td>
+</tr>
+
+</table>
+</form> 
+</body> 
+</html> 
